97a8959663ebd03636d51d74321eee79541e6f94
Destiny Zinke <dzinke@student.neumont.edu>
2019-07-02 11:17:03 -0600

    Found Isolation Solution
    Bugs:
    Namematch doesnt match from contact string
    
    Solution:
    Used .replace to replace phone number and name with empty strings

--- CWDirectory/Program.cs
+++ CWDirectory/Program.cs
@@ -7,22 +7,27 @@ namespace CWDirectory
     {
         public static void Main(string[] args)
         {
-            string contactString = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n";
-
-            string phonePattern = @"^(\/)?(\+?)(1?\([0-9]{3}\)( |)|(1-|1)?[0-9]{3}-?)[0-9]{3}-?[0-9]{4}";
+            string phonePattern = @"^(\+?)(1?\([0-9]{3}\)( |)|(1-|1)?[0-9]{3}-?)[0-9]{3}-?[0-9]{4}";
             string namePattern = @"^(\<)([a-zA-Z])+( |)([a-zA-Z][\']?[a-zA-Z]?)+(\>)";
-            MatchCollection phoneMatch = Regex.Matches("/+1-541-754-3010 156 Alphand_St. <J Steeve>\n", phonePattern);
-            MatchCollection nameMatch = Regex.Matches("<Peter O'Brian>", namePattern);
-            Console.WriteLine("{0} matches", phoneMatch.Count);
-            Console.WriteLine("{0} matches", nameMatch.Count);
+            string contactString = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n";
+            contactString = contactString.Replace("/", "");
+            contactString = contactString.Replace("\n","");
+            Console.WriteLine(contactString);
 
-            char[] temp = phoneMatch[0].ToString().ToCharArray();
+            Match phoneMatch = Regex.Match(contactString, phonePattern);
+
+            //contactString = contactString.Replace(phoneMatch[0].Value + " ", "");
+            MatchCollection nameMatch = Regex.Matches(contactString, namePattern);
+            
+            //contactString = contactString.Replace(nameMatch[0].Value, "");
+            Console.WriteLine(contactString);            
+            //char[] temp = phoneMatch[0].ToString().ToCharArray();
 
 
-            string[] temp2 = contactString.Split(;
+            //string[] temp2 = contactString.Split();
 
-            Console.WriteLine(temp2[0]);
-            Console.WriteLine(temp2[1]);
+            //Console.WriteLine(temp2[0]);
+            //Console.WriteLine(temp2[1]);
         }
     }
 }
