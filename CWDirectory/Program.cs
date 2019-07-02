ad7bc45425bb40946f5a724e95be98bc50331591
Destiny Zinke <dzinke@student.neumont.edu>
2019-07-01 18:46:43 -0600

    Project Files

--- CWDirectory/Program.cs
+++ CWDirectory/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace CWDirectory
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string contactString = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n";
+
+            string phonePattern = @"^(\/)?(\+?)(1?\([0-9]{3}\)( |)|(1-|1)?[0-9]{3}-?)[0-9]{3}-?[0-9]{4}";
+            string namePattern = @"^(\<)([a-zA-Z])+( |)([a-zA-Z][\']?[a-zA-Z]?)+(\>)";
+            MatchCollection phoneMatch = Regex.Matches("/+1-541-754-3010 156 Alphand_St. <J Steeve>\n", phonePattern);
+            MatchCollection nameMatch = Regex.Matches("<Peter O'Brian>", namePattern);
+            Console.WriteLine("{0} matches", phoneMatch.Count);
+            Console.WriteLine("{0} matches", nameMatch.Count);
+
+            char[] temp = phoneMatch[0].ToString().ToCharArray();
+
+
+            string[] temp2 = contactString.Split(;
+
+            Console.WriteLine(temp2[0]);
+            Console.WriteLine(temp2[1]);
         }
     }
 }
